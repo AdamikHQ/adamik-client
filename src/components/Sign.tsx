a0d76b94f2c89443c1d9907d7d553e16890f21cf
fabrice-adamik <159912605+fabrice-adamik@users.noreply.github.com>
2024-05-22 13:07:18 +0200

    ADV-71
    Reduced size of the modal and added scroll bar

--- src/components/Sign.tsx
+++ src/components/Sign.tsx
@@ -4,7 +4,7 @@ import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { Textarea } from "./ui/textarea";
 import { IWallet, Transaction } from "~/types";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { getForm } from "./encode/EncodeForm";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
@@ -40,15 +40,15 @@ export const Sign: React.FC<SignProps> = ({
 
   return (
     <Tabs defaultValue="account">
-      <TabsList className="grid w-full grid-cols-2 mt-2 mb-6">
+      <TabsList className="grid w-full grid-cols-2 mt-2 mb-4">
         <TabsTrigger value="simple">Intent</TabsTrigger>
         <TabsTrigger value="advanced">Advanced</TabsTrigger>
       </TabsList>
       <Card className="overflow-hidden">
-        <CardHeader className="flex flex-row items-start bg-muted/50">
+        <CardHeader className="flex flex-row items-start bg-muted/50 p-4">
           <div className="grid gap-0.5">
-            <CardTitle className="group flex items-center gap-2 text-lg">Information</CardTitle>
-            <CardDescription>
+            <CardTitle className="group flex items-center gap-2 text-sm">Information</CardTitle>
+            <CardDescription className="text-xs">
               Your transaction is now ready to be signed.
               <br />
               The signature will be applied by your browser extension.
@@ -58,14 +58,14 @@ export const Sign: React.FC<SignProps> = ({
             </CardDescription>
           </div>
         </CardHeader>
-        <TabsContent value="simple">
-          <CardContent className="p-6 text-sm">
-            <div className="grid gap-3">
+        <TabsContent value="simple" className="overflow-y-auto max-h-[50vh]">
+          <CardContent className="p-4 text-xs">
+            <div className="grid gap-2">
               {Object.keys(form).length > 0 &&
                 form[transaction.mode].map(({ id, label, value }) => {
                   return (
                     <div key={`${id}-${transaction.mode}`}>
-                      <Label htmlFor={id} key={`${id}-${transaction.mode}-label`}>
+                      <Label htmlFor={id} key={`${id}-${transaction.mode}-label`} className="text-xs">
                         {label}
                       </Label>
                       <Input
@@ -75,11 +75,24 @@ export const Sign: React.FC<SignProps> = ({
                         readOnly={true}
                         placeholder={label}
                         value={value}
+                        className="text-xs h-8"
                       />
                     </div>
                   );
                 })}
-              <Label htmlFor="fees" key={`fees-label`}>
+              <Label htmlFor="sendMax" key={`sendMax-label`} className="text-xs">
+                Send Max
+              </Label>
+              <Input
+                disabled={true}
+                id="sendMax"
+                key="sendMax-input"
+                readOnly={true}
+                placeholder="Send Max"
+                value={transaction.sendMax ? 'true' : 'false'}
+                className="text-xs h-8"
+              />
+              <Label htmlFor="fees" key={`fees-label`} className="text-xs">
                 Fees
               </Label>
               <Input
@@ -89,8 +102,9 @@ export const Sign: React.FC<SignProps> = ({
                 readOnly={true}
                 placeholder="Fees"
                 value={transaction.fees}
+                className="text-xs h-8"
               />
-              <Label htmlFor="gas" key={`gas-label`}>
+              <Label htmlFor="gas" key={`gas-label`} className="text-xs">
                 Gas
               </Label>
               <Input
@@ -100,13 +114,14 @@ export const Sign: React.FC<SignProps> = ({
                 readOnly={true}
                 placeholder="Gas"
                 value={transaction.gas}
+                className="text-xs h-8"
               />
             </div>
           </CardContent>
         </TabsContent>
-        <TabsContent value="advanced">
-          <CardContent className="p-6 text-sm">
-            <div className="grid gap-3">
+        <TabsContent value="advanced" className="overflow-y-auto max-h-[50vh]">
+          <CardContent className="p-4 text-xs">
+            <div className="grid gap-2">
               {encodedTransaction && (
                 <Textarea
                   value={
@@ -115,15 +130,16 @@ export const Sign: React.FC<SignProps> = ({
                       : encodedTransaction
                   }
                   readOnly={true}
+                  className="text-xs"
                 />
               )}
             </div>
           </CardContent>
         </TabsContent>
-        <CardFooter className="flex flex-row items-center justify-end border-t bg-muted/50 px-6 py-3">
-          <Button size="sm" variant="outline" className="h-8 gap-1" onClick={() => signWithWallet()}>
-            <SquarePen className="h-3.5 w-3.5" />
-            <span className="">
+        <CardFooter className="flex flex-row items-center justify-end border-t bg-muted/50 px-4 py-2">
+          <Button size="sm" variant="outline" className="h-6 gap-1" onClick={() => signWithWallet()}>
+            <SquarePen className="h-3 w-3" />
+            <span className="text-xs">
               {`Sign ${wallet.withoutBroadcast === true ? "and broadcast" : ""} with ${wallet.name}`}
             </span>
           </Button>
